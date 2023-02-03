330c7cf9327dd457791fcac6ea1940243b8ef37a
LinkerXXX <89514740+LinkerXXX@users.noreply.github.com>
2023-02-03 14:13:01 +0300

    Admin branch (#2)
    * Добавлена административная панель + создан суперюзер
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * readme
    
    * readme_v2
    
    * Исправил картинку
    
    * Картинка с новыми полями
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Андрей Николаевич Позднухов <Pozdnuhov@avt.local>

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # job_board
+
+
+![Screnshot](https://github.com/LinkerXXX/Job_board/blob/0755af7030604058268bedad2af1119890b2f740/docs/picture/db/Data_base.png)
+
+
+
--- src/board/admin.py
+++ src/board/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from board.models import Vacancy, Company, Specialization
 
 # Register your models here.
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ("name", "specialization", "company", "skills", "description", "salary_max", "salary_min",)
+    search_fields = ("^company",)
+    list_filter = ("created_at", "specialization",)
+    ordering = ("updated_at",)
+    readonly_fields = ("created_at", "updated_at")
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("name", "city", "description", "employee_count")
+    search_fields = ("^name",)
+    list_filter = ("city",)
+    ordering = ("name",)
+
+@admin.register(Specialization)
+class SpecializationAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    search_fields = ("^name",)
+    list_filter = ("name",)
+    ordering = ("name",)
