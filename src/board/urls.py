3ef521bb8b95789d098fc941602e7d8ced39625d
Андрей Николаевич Позднухов <Pozdnuhov@avt.local>
2023-02-28 15:46:20 +0300

    8, 9, 10, 11 пункты

--- src/board/urls.py
+++ src/board/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 # from board.views import get_board_info
-from board.views import IndexView
+from board.views import IndexView, VacancyListView, SpecializationListView, CompanyListView
 
 urlpatterns = [
     # path("all/", get_board_info, name="main_page")
-    path("", IndexView.as_view(), name="main_page")
-
+    path("", IndexView.as_view(), name="main_page"),
+    path("vacancy_list/", VacancyListView.as_view(), name="vacancy_page"),
+    path("spec_list/<int:pk>/",SpecializationListView.as_view(), name = "vacancy_spec_page"),
+    path("company_list/<int:pk>/",CompanyListView.as_view(), name= "company_page")
 ]
--- src/board/views.py
+++ src/board/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from board.models import Specialization, Company, Vacancy
-
+from django.views import View
 # Create your views here.
 
 #class SpecializationListView(ListView):
@@ -14,15 +14,14 @@ from board.models import Specialization, Company, Vacancy
 #    template_name = 'board/main_info.html'
 
 
-def get_board_info(request):
-    template_name = 'board/base.html'
-    all_company = Company.objects.all()
-    all_specialization = Specialization.objects.all()
-    context = {"companies": all_company, 'specializations': all_specialization}
-    return render(request, template_name, context)
+#def get_board_info(request):
+#    template_name = 'board/base.html'
+#    all_company = Company.objects.all()
+#    all_specialization = Specialization.objects.all()
+#    context = {"companies": all_company, 'specializations': all_specialization}
+#    return render(request, template_name, context)
 
 
-from django.views import View
 
 class IndexView(View):
     template_name = 'board/base.html'
@@ -40,3 +39,41 @@ class IndexView(View):
         return context
         
 
+class VacancyListView(ListView):
+    template_name = "board/vacancy_list.html"
+    queryset = Vacancy.objects.all()
+    
+
+class SpecializationListView(DetailView):
+    template_name = "board/specialization_list.html"
+    queryset = Specialization.objects.all()
+    
+
+class CompanyListView(DetailView):
+    template_name = "board/company_list.html"
+    queryset = Company.objects.all()
+
+#    def get(self, request, *args, **kwargs):
+#        context = self.get_context_data(*args, **kwargs)
+#        return render(request, self.template_name, context)
+#
+#    def get_context_data(self, *args, **kwargs):
+#        all_specialization = Specialization.objects.all()
+#        all_vacancy = Vacancy.objects.all()
+#        context = {'specializations': all_specialization, "vacancies":all_vacancy}
+#        return context
+
+#class  VacancyListView(View):
+#    template_name = "board/vacancy_list.html"
+#
+#    def get(self, request, *args, **kwargs):
+#        context = self.get_context_data(*args, **kwargs)
+#        return render(request, self.template_name, context)
+#
+#
+#    def get_context_data(self, *args, **kwargs):
+#        all_company = Company.objects.all()
+#        all_specialization = Specialization.objects.all()
+#        all_vacancy = Vacancy.objects.all()
+#        context = {"companies": all_company, 'specializations': all_specialization, "vacancies":all_vacancy}
+#        return context
