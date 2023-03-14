de19595107faed11a5b699c78e861c1e35f95981
LinkerXXX <89514740+LinkerXXX@users.noreply.github.com>
2023-03-14 11:44:37 +0300

    Templates_integration
    Добавлена административная панель.
    Добавлены шаблоны и соответсвующие им view-классы.
    Обновлен Read-me файл.
    ---------
    
    Co-authored-by: Андрей Николаевич Позднухов <Pozdnuhov@avt.local>
    Co-authored-by: AVT\Pozdnuhov <anp@ea.com>

--- src/board/views.py
+++ src/board/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from board.models import Specialization, Company, Vacancy
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    template_name = "board/index.html"
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(*args, **kwargs)
+        return render(request, self.template_name, context)
+
+    def get_context_data(self, *args, **kwargs):
+        all_company = Company.objects.all()
+        all_specialization = Specialization.objects.all()
+        context = {"companies": all_company, "specializations": all_specialization}
+        return context
+
+
+class VacancyListView(ListView):
+    template_name = "board/vacancy_list.html"
+    queryset = Vacancy.objects.all()
+
+
+class SpecializationListView(DetailView):
+    template_name = "board/specialization_list.html"
+    queryset = Specialization.objects.all()
+
+
+class CompanyListView(DetailView):
+    template_name = "board/company_list.html"
+    queryset = Company.objects.all()
--- src/job_board/settings.py
+++ src/job_board/settings.py
@@ -12,85 +12,86 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
+
 
 env = environ.Env()
-environ.Env.read_env('../conf/default.env')
-SECRET_KEY = env('SECRET_KEY')
+environ.Env.read_env("../conf/default.env")
+SECRET_KEY = env("SECRET_KEY")
 
-DEBUG = env('DEBUG')
+DEBUG = env("DEBUG")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    "board"
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "board",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'job_board.urls'
+ROOT_URLCONF = "job_board.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'job_board.wsgi.application'
-
+WSGI_APPLICATION = "job_board.wsgi.application"
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = "ru"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -100,18 +101,20 @@ USE_TZ = True
 SHORT_DATE_FORMAT = "j.m.Y"
 TIME_FORMAT = "H:i:s"
 DATE_INPUT_FORMATS = ["%d.%m.%Y"]
-TIME_INPUT_FORMATS = ['%H:%M:%S']
+TIME_INPUT_FORMATS = ["%H:%M:%S"]
 FIRST_DAY_OF_WEEK = 1
 DATE_FORMAT = "j E Y"
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
+MEDIA_URL = "/media/"
+# MEDIA_ROOT = 'C:/Users/Pozdnuhov/Desktop/Python_work/images'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'C:/Users/Pozdnuhov/Desktop/Python_work/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-MEDIA_COMPANY_IMAGE_DIR = 'C:/Users/Pozdnuhov/Desktop/Python_work/images/company'
-MEDIA_SPECIALITY_IMAGE_DIR = 'C:/Users/Pozdnuhov/Desktop/Python_work/images/specialization'
+
+MEDIA_COMPANY_IMAGE_DIR = "media/company/"
+MEDIA_SPECIALITY_IMAGE_DIR = "media/specialization/"
